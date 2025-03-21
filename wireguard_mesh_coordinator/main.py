a2a67645623c0fc139e9f3d9b3fb0aa81a65e146
DemyCode <verycols@gmail.com>
2025-03-21 19:35:13 +0100

    fix: only api and enter network

--- wireguard_mesh_coordinator/main.py
+++ wireguard_mesh_coordinator/main.py
@@ -1,12 +1,9 @@
 import os
 from wireguard_mesh_coordinator.api import serve
 from wireguard_mesh_coordinator.generate_new_machine_config import generate_config
-from wireguard_mesh_coordinator.command import (
-    add_to_all_peers_and_myself,
-    generate_next_ip_func,
-)
+from wireguard_mesh_coordinator.command import generate_next_ip_func
 from typer import Typer
-from wireguard_mesh_coordinator.utils import NewPeer, wg_quick_parser
+from wireguard_mesh_coordinator.utils import wg_quick_parser
 
 app = Typer()
 
@@ -16,7 +13,6 @@ def api():
     serve()
 
 
-@app.command()
 def generate_new_machine_config(
     peer_ip: str, next_internal_ip: str, remote_config: str, output: str
 ):
@@ -25,30 +21,6 @@ def generate_new_machine_config(
     new_config = generate_config(peer_wire_guard_config, peer_ip, next_internal_ip)
     with open(output, "w") as file:
         file.write(str(new_config))
-
-
-@app.command()
-def generate_next_ip():
-    # will run on the machine that wants to join the mesh
-    with open("/etc/wireguard/wg0.conf", "r") as file:
-        wireguard_config = wg_quick_parser(file.read())
-    print(generate_next_ip_func(wireguard_config), end="")
-
-
-@app.command()
-def register_and_propagate_new_machine(
-    public_key: str, allowed_ips: str, endpoint: str
-):
-    add_to_all_peers_and_myself(
-        NewPeer(public_key=public_key, allowed_ips=allowed_ips, endpoint=endpoint)
-    )
-
-
-@app.command()
-def read_wireguard_private_key():
-    with open("/etc/wireguard/wg0.conf", "r") as file:
-        wireguard_config = wg_quick_parser(file.read())
-    print(wireguard_config.interface.private_key)
 
 
 @app.command()
